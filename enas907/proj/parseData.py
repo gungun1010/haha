b4fc7088bd72d01f8e0f8c1e1dcb123861612b56
gungun1010 <gungun1010@Leons-Air-2.wireless.yale.internal>
2014-12-12 02:00:13 -0500

    trainning working

--- enas907/proj/parseData.py
+++ enas907/proj/parseData.py
@@ -18,13 +18,17 @@ import time
 import xxhash
 import struct
 
-NUM_COLS = 40
-NUM_ITR = 10
+NUM_COLS = 60
+NUM_ITR = 100
 
 class NestedDict(dict):
     def __getitem__(self, key):
         if key in self: return self.get(key)
         return self.setdefault(key, NestedDict())
+
+def split_hex(text):
+    text = text.zfill(len(text) + len(text) % 16)
+    return ' '.join(text[i: i+16] for i in range(0, len(text), 16))
 
 def sklearnSGD():
     global trainData
@@ -33,7 +37,7 @@ def sklearnSGD():
     global testLabel
     
     trainData, trainLabel = sklearn.datasets.make_classification(
-        n_samples=10000, n_features=4, n_redundant=0, n_informative=2, 
+        n_samples=14300, n_features=NUM_COLS, n_redundant=0, n_informative=2, 
         n_clusters_per_class=2, hypercube=False, random_state=0
     )
 
@@ -46,20 +50,21 @@ def sklearnSGD():
     #_=pd.scatter_matrix(df, figsize=(9, 9), diagonal='kde', marker='o', s=40, alpha=.4, c=y[ind])
 
     # Train and test the scikit-learn SGD logistic regression.
-    clf = sklearn.linear_model.SGDClassifier(
-        loss='log', n_iter=1000, penalty='l2', alpha=1e-3, class_weight='auto')
+    #clf = sklearn.linear_model.SGDClassifier(
+     #   loss='log', n_iter=1000, penalty='l2', alpha=1e-3, class_weight='auto')
 
-    clf.fit(trainData, trainLabel)
-    testLabel_pred = clf.predict(testData)
-    print('Accuracy: {:.3f}'.format(sklearn.metrics.accuracy_score(testLabel, testLabel_pred)))
+    #clf.fit(trainData, trainLabel)
+    #testLabel_pred = clf.predict(testData)
+    #print('Accuracy: {:.3f}'.format(sklearn.metrics.accuracy_score(testLabel, testLabel_pred)))
 
 def parseProcess():
     global trainData, trainLabel, testData, testLabel
     
     trainData = np.array([],dtype=float)
-    trainLabel = np.array([],dtype=int)
+    procInfoHex = []
+    #trainLabel = np.array([],dtype=int)
     testData = np.array([], dtype=float)
-    testLabel = np.array([], dtype=int)
+    #testLabel = np.array([], dtype=int)
 
     #entry[0] = ['USER', 'PID', '%CPU', '%MEM', 'VSZ', 'RSS', 'TTY', 'STAT', 'START', 'TIME', 'COMMAND']
     #USER(0), %CPU(2), %MEM(3), VSZ(4), RSS(5), TIME(9), CMD(10) are important attribute
@@ -72,38 +77,44 @@ def parseProcess():
         #print len(entry)
 
         for indx in range(len(entry)):
+            hexList = []
             if indx != 0:
                 procInfo = entry[indx].split() #split array into elements
-
-                #if there is total number of parameters is less than 5, fill in with blanks
-                while len(procInfo) < NUM_COLS:
-                    procInfo.append(' ')
                 
                 #remove meaningless contents
                 procInfo.pop(1);
                 procInfo.pop(5);
                 procInfo.pop(6);
                 #procInfo = ['USER', '%CPU', '%MEM', 'VSZ', 'RSS','STAT','TIME', 'COMMAND','param0', 'param1', 'param2', 'param3', 'param4', .... 'paramN']
-                #print procInfo
-                #hash in xxhash (non-cryto hashing) then convert to double
-                procInfoHash = [ struct.unpack('l', xxhash.xxh64(s).hexdigest().decode('hex'))[0] for s in procInfo]
-                #print procInfo
-                #print procInfoHash
+                #convert plain text string to 8-byte element a pop
+                for s in procInfo:
+                    #if an attribute is more than 8 bytes, we split it
+                    procInfoHex = split_hex(s.encode('hex'))
+                    procInfoHex = procInfoHex.split()
+                    hexList= hexList + procInfoHex
+                
+                
+                #the buffer is 60 doubles wide, so padding the remainders with ' '
+                while len(hexList) < NUM_COLS:
+                    hexList.append(' '.encode('hex'))
+                
+                #covert Hex to double
+                procInfoHash = [ struct.unpack('<d', s.zfill(16).decode('hex'))[0] for s in hexList]
 
                 if indx == 1 and init == 1:
                     init=0 #only init numpy once
                     
                     #procInfo[7] is the command, use it as label
                     trainData=np.hstack((trainData, np.float_(procInfoHash)));
-                    trainLabel=np.append(trainLabel, np.int_(procInfoHash[7]));
+                    #trainLabel=np.append(trainLabel, np.int_(procInfoHash[7]));
 
                 else:
                     trainData=np.vstack((trainData, np.float_(procInfoHash)));
-                    trainLabel=np.append(trainLabel, np.int_(procInfoHash[7]));
+                    #trainLabel=np.append(trainLabel, np.int_(procInfoHash[7]));
         iteration += 1
     
     # Split into train and test
-    trainData, testData, trainLabel, testLabel = sklearn.cross_validation.train_test_split(trainData, trainLabel)
+    trainData, testData= sklearn.cross_validation.train_test_split(trainData)
 
 def genH5():
     global dirname, train_filename, test_filename
@@ -143,23 +154,41 @@ def trainDNN():
 def main():
     global trainData, trainLabel, testData, testLabel, dirname, train_filename, test_filename 
     
+    sklearnSGD()
+    print "this is working dataset -----------------------------------------"
+    print "trainData: "
+    print trainData
+    print trainData.shape
+    print "trainLabel: "
+    print trainLabel
+    print trainLabel.shape
+    print "testData: "
+    print testData
+    print testData.shape
+    print "testLabel: "
+    print testLabel
+    print testLabel.shape
+
     parseProcess()
+    print "this is working dataset -----------------------------------------"
+    print "trainData: "
+    print trainData
+    print trainData.shape
+    print "trainLabel: "
+    print trainLabel
+    print trainLabel.shape
+    print "testData: "
+    print testData
+    print testData.shape
+    print "testLabel: "
+    print testLabel
+    print testLabel.shape
     #sys.exit()
-    #sklearnSGD()
 
 
     genH5()
 
     zipH5()
-    print trainLabel.shape
-    print trainData.shape
-
-    print "data type: "+str(trainData.dtype)
-    print "label type: "+str(trainLabel.dtype)
-
-    print testLabel.shape
-    print testData.shape
-    #sys.exit()
     
     #FIXME try train tomorrow
     trainDNN()
